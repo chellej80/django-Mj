3c28c806db100a8ed74385b7db938310dfe0e707
chellej80 <embarrequestrian@gmail.com>
2022-06-23 20:30:50 +0000

    update

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todolist
+from todo.views import get_todolist, add_item
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todolist, name='get_todolist')
+    path('', get_todolist, name='get_todolist'),
+    path('add', add_item, name='add')
+
 ]
